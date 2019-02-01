e11487f27bff080faaabc0e66129736fff86816d
Nivedita Gautam <niveditagautam000@gmail.com>
2019-02-01 01:21:52 -0600

    Update Main.java

--- code/Main.java
+++ code/Main.java
@@ -10,38 +10,30 @@ public class Main {
 
 		System.out.println(game.getScore());
 		System.out.println(game.isFinished());
-		
-		
-		game.roll(0);
-		
-		
-		for (int i = 0; i < 17 ; i++) {
-			game.roll(0);
-		}
-		
-		game.roll(10);
-		game.roll(9);
-		game.roll(1);
 
-		// Score board
-		for (int i = 0 ; i < game.frames.length; i++) {
-			
-			System.out.print(i+". ");
+		for (int i = 0; i < 12; i++) {
+			game.roll(10);
+		}
+
+		System.out.println(game.getScore());
+		System.out.println(game.isFinished());
+
+		// printing Score board
+		for (int i = 0; i < game.frames.length; i++) {
+
+			System.out.print("Attempt "+ i + " - ");
 			System.out.print("[" + game.frames[i].getFirstRoll() + " , ");
-			
+
 			if (game.frames[i].isSpare())
 				System.out.print("\\");
 			else if (game.frames[i].isStrike())
 				System.out.print("X");
-			else 
+			else
 				System.out.print(game.frames[i].getSecondRoll());
 
 			System.out.print("] , Temp: " + game.frames[i].getTemp());
 			System.out.print(", Points: " + game.frames[i].getPoints());
 			System.out.println();
 		}
-		
-		System.out.println(game.getScore());
-		System.out.println(game.isFinished());
 	}
 }
